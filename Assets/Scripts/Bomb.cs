1d1cc989dde6b52ec635162ab15439f7e7b8e4b9
Milan Stanojlovic <milanovmejl@yahoo.com>
2018-11-15 01:24:55 +0100

    Second Commit

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public int firePower;
+    public float fuse;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Explode", fuse);
+	}
+	
+	void Explode() {
+        Debug.Log("BOOM " + firePower);
+        Destroy(gameObject);
+	}
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        GetComponent<BoxCollider2D>().isTrigger = false;
+    }
+}
---
+++ Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour{
+    public GameObject bomb;
+    int numberOfBombs = 1;
+    
+    public int firePower = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump") && numberOfBombs >= 1)
+        {
+            Vector2 spawnPos = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+            var newBomb =  Instantiate(bomb, spawnPos, Quaternion.identity) as GameObject;
+            newBomb.GetComponent<Bomb>().firePower = firePower;
+            numberOfBombs--;
+            Invoke("AddBomb", 1f);
+        }
+	}
+
+    public void AddBomb()
+    {
+        numberOfBombs++;
+    }
+}
